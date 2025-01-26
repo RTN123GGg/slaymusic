78f2dde90e208582a1224bb724e8cef1031c562a
RTN123GGg <rioinvesting9@gmail.com>
2025-01-26 04:30:44 +0100

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
   commandsDir: './commands',  
   embedColor: "#1db954",
   activityName: "MUSIC FOR RIO", 
-  activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
+  activityType: "STRAMING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/xQF9f9yUEM",
   embedTimeout: 5, 
   errorLog: "", 
